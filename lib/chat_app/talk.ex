159034f960f0566acd4906dedde51aa4e36fccc8
Polarbear-289 <sm8093@srmist.edu.in>
2022-02-23 20:13:53 +0530

    first commit

---
+++ lib/chat_app/talk.ex
@@ -0,0 +1,27 @@
+defmodule ChatApp.Talk do
+    alias ChatApp.Repo
+    alias ChatApp.Talk.Room
+    def list_rooms do
+        Repo.all(Room)
+    end
+    def change_room(%Room{} = room) do
+        Room.changeset(room, %{})
+    end
+    def create_room(user,attrs \\ %{}) do
+        user
+        |> Ecto.build_assoc(:rooms)
+        |> Room.changeset(attrs)
+        |> Repo.insert()
+    end
+    def update_room(%Room{}= room, attrs) do
+        room
+        |> Room.changeset(attrs)
+        |> Repo.update()
+    end
+    def get_room!(id), do: Repo.get!(Room, id)
+    
+    def delete_room(%Room{} = room) do
+        room |> Repo.delete()
+      end
+end
+  
